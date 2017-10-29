2778c9a94562017644a8327365e5f43e6a6373f1
white rabbit <whiterabbit@peacefulnukes.com>
2017-10-29 19:56:13 +0100

    adds

--- request.py
+++ request.py
@@ -4,7 +4,7 @@ import random
 
 async def request_to_future(future, session, *session_args, **session_kwargs):
     async with session.request(*session_args, **session_kwargs) as resp:
-        future.set_result(resp)
+        future.set_result(await resp.json())
 
 http = aiohttp.ClientSession()
 clips = asyncio.Future()
@@ -13,6 +13,5 @@ asyncio.ensure_future(request_to_future(clips, http, 'get', 'https://api.twitch.
             headers = {'Accept': 'application/vnd.twitchtv.v5+json', 'Client-ID': '6agi3wajd7fjqixf5wp9mzof4q8d0b'}))
 
 async def use_clips():
-    await clips
-    json_res = await clips.result().json()
-    return random.choice(json_res['clips'])['url']
+    result =  clips.result()
+    return random.choice(result['clips'])['url']
